85c21944e20cb493cd8ef3a94a971533a77d9727
Luke Edwards <luke.edwards05@gmail.com>
2020-01-20 20:30:12 -0800

    feat: add module logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-export default function () {
-	//
+export default function (target) {
+	var i=0, j=0, week, out=[], date = new Date(target || new Date);
+	var year = date.getFullYear(), month = date.getMonth();
+
+	// day index (of week) for 1st of month
+	var first = new Date(year, month, 1).getDay();
+
+	// how many days there are in this month
+	var days = new Date(year, month+1, 0).getDate();
+
+	while (i < days) {
+		for (j=0, week=new Array(7); j < 7;) {
+			while (j < first) week[j++] = 0;
+			week[j++] = ++i > days ? 0 : i;
+			first = 0;
+		}
+		out.push(week);
+	}
+
+	return out;
 }
